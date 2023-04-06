5ebdfdb13188f1f029bb4859e30074782c3c5c76
matheusslr <matheusslr@hotmail.com>
2023-04-06 19:10:34 -0300

    refactor: naming microservices from the same ecosystem

--- 02_book-service/src/main/java/com/matheusslr/bookservice/service/BookService.java
+++ 02_book-service/src/main/java/com/matheusslr/bookservice/service/BookService.java
@@ -1,6 +1,7 @@
 package com.matheusslr.bookservice.service;
 
 import com.matheusslr.bookservice.model.Book;
+import com.matheusslr.bookservice.proxy.CambioProxy;
 import com.matheusslr.bookservice.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -12,14 +13,19 @@ public class BookService {
     private BookRepository bookRepository;
     @Autowired
     private Environment environment;
+    @Autowired
+    CambioProxy cambioProxy;
 
     public Book findById(Long id, String currency){
         Book book = bookRepository.findById(id).get();
         if(book == null) throw new RuntimeException("Book not found");
 
+        var cambio = cambioProxy.getCambio(book.getPrice(), "USD", currency);
+
         var port = environment.getProperty("local.server.port");
         book.setEnvironment(port);
         book.setCurrency(currency);
+        book.setPrice(cambio.getConvertedValue());
         return book;
     }
 }
