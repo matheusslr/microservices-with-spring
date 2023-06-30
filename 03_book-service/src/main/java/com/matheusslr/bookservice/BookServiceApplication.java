3850e50ad2c9f8b76e890271f2bd7aed8d54a739
matheusslr <matheusslr@hotmail.com>
2023-06-30 13:59:57 -0300

    feat: Adding Observability with Micrometer and Zipkin

--- 03_book-service/src/main/java/com/matheusslr/bookservice/BookServiceApplication.java
+++ 03_book-service/src/main/java/com/matheusslr/bookservice/BookServiceApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 public class BookServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookServiceApplication.class, args);
+    }
 
 }
